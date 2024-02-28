a881341acc0f2fa6a39b5b23a44c4f4da9ed09c8
Federico Di Cillo <dicillo.fe@gmail.com>
2024-02-28 11:38:43 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+import { HeroComponent } from './components/sections/hero/hero.component';
+import { AboutComponent } from './components/sections/about/about.component';
+import { ExperienceComponent } from './components/sections/experience/experience.component';
+import { ProjectsComponent } from './components/sections/projects/projects.component';
+import { EducationComponent } from './components/sections/education/education.component';
+import { SkillsComponent } from './components/sections/skills/skills.component';
+import { KeyboardManagerComponent } from './components/keyboard-manager/keyboard-manager.component';
+
+
+import { basics } from '@cv';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, 
+            RouterOutlet,
+            HeroComponent,
+            AboutComponent,
+            ExperienceComponent,
+            ProjectsComponent,
+            EducationComponent,
+            SkillsComponent,
+            KeyboardManagerComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent implements OnInit {
+ 
+  ngOnInit(): void {
+    document.title = `Portfolio de ${basics.name}`;
+
+  }
+}
---
+++ src/app/components/keyboard-manager/keyboard-manager.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-keyboard-manager',
+  standalone: true,
+  imports: [],
+  templateUrl: './keyboard-manager.component.html',
+  styleUrl: './keyboard-manager.component.css'
+})
+export class KeyboardManagerComponent {
+
+}
