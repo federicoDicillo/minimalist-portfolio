5d1e7a5215fe237b94cf8918d0bd7d496c4782f8
Federico Di Cillo <dicillo.fe@gmail.com>
2024-03-12 10:13:34 -0300

    add education

--- src/app/components/sections/education/education.component.ts
+++ src/app/components/sections/education/education.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { SectionComponent } from '../../section/section.component';
+import { education } from '@cv';
 
 @Component({
   selector: 'app-education',
   standalone: true,
-  imports: [SectionComponent],
+  imports: [CommonModule,
+            SectionComponent],
   templateUrl: './education.component.html',
   styleUrl: './education.component.css'
 })
 export class EducationComponent {
+public education = education
+
 
 }
